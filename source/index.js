841c2a6d403ae3259931b00ebe56acca37efb8ba
Konstantin Konotopchyk <k.konotopchyk@whirl.sg>
2017-09-11 23:50:51 +0300

    final comit5-master-final11

--- source/index.js
+++ source/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
-
-import registerSw from './ServiceWorker'
+// import registerSw from './ServiceWorker'
 import { HashRouter as Router, Route} from 'react-router-dom'
 
 
@@ -14,6 +12,6 @@ ReactDOM.render((
         </div>
     </Router>),
     document.getElementById('root'))
-registerSw();
+// registerSw();
 
 
